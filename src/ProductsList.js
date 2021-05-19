22d44785799fbc15938d0d5b4a62015c65c9fc50
Gene Kelley <eugenek@guildmortgage.net>
2021-05-19 11:42:03 -0700

    Refactor to ProductsList and AddProductForm components.

---
+++ src/ProductsList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Product from "./Product.js";
+
+export default function ProductsList(props) {
+    return <ul className="store-front">
+        {props.products.map(product => <li key={product.id}>
+            <Product details={product} />
+            <button className="btn-outline btn-delete" onClick={() => props.onDeleteClick(product.id)}>Delete</button>
+        </li>)}
+    </ul>;
+}
