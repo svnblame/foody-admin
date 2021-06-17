c8ed5c90f07aae549ce7698d6e0574f65cc7a12d
Gene Kelley <eugenek@guildmortgage.net>
2021-06-17 15:10:38 -0700

    Persist added products to localStorage.

--- src/StoreFront.js
+++ src/StoreFront.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProductsList from "./ProductsList.js";
 import AddProductForm from "./AddProductForm.js";
 
 export default function StoreFront() {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState(() => {
+        const savedProducts = localStorage.getItem('products');
+        if (savedProducts) {
+            return JSON.parse(savedProducts);
+        } else {
+            return [];
+        }
+    });
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [validation, setValidation] = useState("");
+
+    useEffect(() => {
+        if (products.length === 0) {
+            document.title = 'No Products';
+        } else if (products.length === 1) {
+            document.title = '1 Product';
+        } else {
+            document.title = `${products.length} products`
+        }
+    }, [products]);
+
+    useEffect(() => {
+        localStorage.setItem('products', JSON.stringify(products));
+    }, [products]);
 
     function handleFormSubmit(event) {
         event.preventDefault();
